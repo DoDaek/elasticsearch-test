5314004973b6598ac78597a4a48539258f1d8f32
DoDaek <dbxor8960@naver.com>
2019-06-27 11:25:00 +0900

    [EST-1] Add callElasticSearchApi skeleton code

--- src/main/java/com/taek/elasticsearchtest/elasticsearchtest/api/ElasticSearchAPI.java
+++ src/main/java/com/taek/elasticsearchtest/elasticsearchtest/api/ElasticSearchAPI.java
@@ -1,8 +1,17 @@
 package com.taek.elasticsearchtest.elasticsearchtest.api;
 
+import org.elasticsearch.client.RestClient;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class ElasticSearchAPI {
-    
+
+    private RestClient restClient;
+
+    public Map<String, Object> callElasticSearchApi(String method, String url, String jsonData) {
+        return null;
+    }
+
 }
