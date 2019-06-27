22a38a97537cb7a2554fd92df3b4a08b98c779bf
DoDaek <dbxor8960@naver.com>
2019-06-27 10:56:45 +0900

    [EST-1] Add json dependency and remove API's field value

--- src/main/java/com/taek/elasticsearchtest/elasticsearchtest/api/ElasticSearchAPI.java
+++ src/main/java/com/taek/elasticsearchtest/elasticsearchtest/api/ElasticSearchAPI.java
@@ -1,14 +1,8 @@
 package com.taek.elasticsearchtest.elasticsearchtest.api;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ElasticSearchAPI {
-    @Value ("${elasticsearch.host}")
-    private String host;
-    @Value ("${elasticsearch.port}")
-    private int port;
-
-
+    
 }
