d1fca392c9e129224b1e890e8310c0a7a87295fb
DoDaek <dbxor8960@naver.com>
2019-06-27 12:54:23 +0900

    [EST-1] Add ElasticSearchData class

--- src/main/java/com/taek/elasticsearchtest/elasticsearchtest/domain/ElasticSearchData.java
+++ src/main/java/com/taek/elasticsearchtest/elasticsearchtest/domain/ElasticSearchData.java
@@ -1,7 +1,19 @@
 package com.taek.elasticsearchtest.elasticsearchtest.domain;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 public class ElasticSearchData {
+    @Id
+    private String id;
+    private String fileName;
+    private String filePath;
+    // MD5 Hash value in here
+    private Metadata metadata;
+
+    @Data
+    public static class Metadata {
+        // Metadata's values in here
+    }
 }
